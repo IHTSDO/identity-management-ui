5d29ceda5d28b6a4273d368a43ae50ddffd5f286
Darryn McGaw <dmcg@ihtsdo.org>
2024-12-04 16:48:16 +0000

    PIP-435 Rename snomed-sso-ui to identity-management-ui

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to snomed-sso-ui!');
+    expect(page.getTitleText()).toEqual('Welcome to identity-management-ui!');
   });
 });
