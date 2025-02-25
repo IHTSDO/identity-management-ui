1d83f928f684a48b068b4a41c193f9a12557f535
Peter Duff <peterduff@gmail.com>
2025-02-25 12:04:11 +0000

    MAINT-2770 Home page

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../models/user";
+import {Subscription} from "rxjs";
+import {AuthenticationService} from "../../services/authentication/authentication.service";
+import {NgIf} from "@angular/common";
+import {Router, RouterLink} from "@angular/router";
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    imports: [NgIf, RouterLink],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+    user!: User;
+    userSubscription: Subscription;
+
+    constructor(private readonly authenticationService: AuthenticationService, public router: Router) {
+        this.userSubscription = this.authenticationService.getUser().subscribe(data => this.user = data);
+    }
+
+    ngOnInit() {
+        console.log('home user: ', this.user);
+    }
+}
