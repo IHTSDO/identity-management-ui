25cef21f011d7c9cf84cfac2358e57ebffe1061f
Peter Duff <peterduff@gmail.com>
2025-04-04 16:33:28 +0100

    MAINT-2763 Navbar styling and links

--- src/app/components/snomed-navbar/snomed-navbar.component.ts
+++ src/app/components/snomed-navbar/snomed-navbar.component.ts
@@ -2,13 +2,13 @@ import {Component} from '@angular/core';
 import {User} from "../../models/user";
 import {Subscription} from "rxjs";
 import {AuthenticationService} from "../../services/authentication/authentication.service";
-import {NgIf} from "@angular/common";
+import {NgFor, NgIf} from "@angular/common";
 import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-snomed-navbar',
     standalone: true,
-    imports: [NgIf],
+    imports: [NgIf, NgFor],
     templateUrl: './snomed-navbar.component.html',
     styleUrl: './snomed-navbar.component.scss'
 })
@@ -20,9 +20,11 @@ export class SnomedNavbarComponent {
     expandedUserMenu: boolean = false;
     expandedAppMenu: boolean = false;
     expandedItemMenu: boolean = false;
+    rolesView: boolean = false;
 
-    constructor(private authenticationService: AuthenticationService, private router: Router) {
+    constructor(private readonly authenticationService: AuthenticationService, private readonly router: Router) {
         this.userSubscription = this.authenticationService.getUser().subscribe(data => this.user = data);
+        router.events.subscribe(() => this.closeMenus());
     }
 
     switchMenu(name: string): void {
@@ -45,14 +47,38 @@ export class SnomedNavbarComponent {
         }
     }
 
+    closeMenus(): void {
+        this.expandedUserMenu = false;
+        this.expandedAppMenu = false;
+        this.expandedItemMenu = false;
+        this.rolesView = false;
+    }
+
+    openRolesView(): void {
+        this.closeMenus();
+        this.rolesView = true;
+    }
+
+    getInitials(user: User): string {
+        return user.firstName.charAt(0).toUpperCase() + user.lastName.charAt(0).toUpperCase();
+    }
+
+    navigateTo(location: string): void {
+        this.router.navigate([location]);
+    }
+
+    redirectTo(url: string): void {
+        window.open(url, '_blank');
+    }
+
     logout(): void {
         this.authenticationService.httpLogout().subscribe({
-            next: (v) => {
+            next: () => {
+                console.log('navbarComp.logout');
                 this.authenticationService.setUser(undefined!);
                 this.router.navigate(['/login']);
             },
-            error: (e) => console.error('error: ', e),
-            complete: () => console.info('complete')
+            error: (e) => console.error('error: ', e)
         });
     }
 }
