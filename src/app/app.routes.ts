9bf79d6b1927e578972c7e1db381655adab47513
Quyen Ly <quocquyenit@gmail.com>
2025-09-03 19:24:41 +0700

    PIP-889 Remove security page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {UserInformationComponent} from "./components/user-information/user-information.component";
-import {SecurityComponent} from "./components/security/security.component";
 import {authenticationGuard} from "./guards/authentication.guard";
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent, canActivate:[authenticationGuard]},
     {path: 'user-information', component: UserInformationComponent, canActivate:[authenticationGuard]},
-    {path: 'security', component: SecurityComponent, canActivate:[authenticationGuard]},
     {path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
